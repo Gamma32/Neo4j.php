5c06afccbcb1bf187323c85eb131c2605c59dc4a
Jacob Hansson <jacob@bitchware.se>
2010-10-08 00:13:03 +0200

    Merged traversal. Working on fixing resulting bugs.

--- NeoRest.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once 'NeoRest/HttpHelper.php';
-require_once 'NeoRest/NeoRestHttpException.php';
-require_once 'NeoRest/NotFoundException.php';
-require_once 'NeoRest/PropertyContainer.php';
-require_once 'NeoRest/Node.php';
-require_once 'NeoRest/Relationship.php';
-require_once 'NeoRest/GraphDatabaseService.php';
